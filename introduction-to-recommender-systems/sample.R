56e5193087adb627500f922bda1bfbca50ed3113
Arthur Grava <arthur.grava@gmail.com>
2017-06-05 13:55:59 -0300

    :zap: Adding a sample R script using all of features developed until now

--- introduction-to-recommender-systems/sample.R
+++ introduction-to-recommender-systems/sample.R
@@ -1,7 +1,21 @@
 BASE_DIR = "/Users/arthur/development/personal/git/teaching"
 
+# add custom functions
+source(paste(BASE_DIR, "introduction-to-recommender-systems/popularity.R", sep="/"))
+source(paste(BASE_DIR, "introduction-to-recommender-systems/ratings.R", sep="/"))
+source(paste(BASE_DIR, "introduction-to-recommender-systems/wxax.R", sep="/"))
+source(paste(BASE_DIR, "introduction-to-recommender-systems/imprime.R", sep="/"))
+
+# read sample file
 data = read.csv(
     paste(BASE_DIR, "data/sample.csv", sep="/")
 )
 
-colnames(data)
+populares = popularidade(data)
+avaliados = rankingAvaliacoes(data)
+avaliadosNormalizado = rankingAvaliacoesNormalizado(data)
+wvav = qxax(data)
+
+imprime(populares)
+imprime(avaliados)
+imprime(avaliadosNormalizado)
